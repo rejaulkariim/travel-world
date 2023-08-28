eb39faee487eaae9283286e312b6081f8c90b7af
jannatun-nur <shadrinmoni15@gmail.com>
2023-08-28 12:55:26 +0600

    Slider added

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,13 +1,17 @@
 import AboutSection from "./About";
 import CallToActionSection from "./CallToAction";
 import ContactSection from "./Contact";
+import HeroSlider from "./HeroSlider";
 import ServicesSection from "./Services";
 import TestimonialsSection from "./Testimonials";
 import TravelTipsSection from "./TravelTips";
 
+
 const Hero = () => {
+
   return (
     <div className="mt-20 wrapper">
+      <HeroSlider />
       <ServicesSection />
       <AboutSection />
       <TravelTipsSection />
